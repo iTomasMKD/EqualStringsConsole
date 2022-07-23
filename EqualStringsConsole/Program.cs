51d7b26864580a77c4e0b1cc811a287d3c4058a4
Ilijevski7 <tomche.kubikos@hotmail.com>
2022-07-23 12:27:41 +0200

    Add project files.

---
+++ EqualStringsConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EqualStringsConsole
+{
+    class Program
+    {
+        public bool FindEqual(string a, string b)
+        {
+            if (a == b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+            static void Main(string[] args)
+        {
+            string a = "y";
+            string b = "y";
+            Program pr = new Program();
+            bool z = pr.FindEqual(a, b);
+            Console.WriteLine(z);
+        }
+    }
+}
