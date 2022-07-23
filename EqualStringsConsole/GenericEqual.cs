c64c62377440d164ae14df110be351dd074a2364
Ilijevski7 <tomche.kubikos@hotmail.com>
2022-07-23 12:44:36 +0200

    Add genereic and lambda call

---
+++ EqualStringsConsole/GenericEqual.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualStringsConsole
+{
+    public class GenericEqual
+    {
+        public static bool FindEqual(string a, string b)
+        {
+            if (a == b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- EqualStringsConsole/Program.cs
+++ EqualStringsConsole/Program.cs
@@ -17,11 +17,23 @@ namespace EqualStringsConsole
         }
             static void Main(string[] args)
         {
+            
             string a = "y";
             string b = "y";
+
+            //instance call
             Program pr = new Program();
             bool z = pr.FindEqual(a, b);
             Console.WriteLine(z);
-        }
+
+            //generic call extension method
+            Func<string, string, bool> equal = GenericEqual.FindEqual;
+            bool x = equal(a, b);
+            Console.WriteLine(x);
+
+            //using lambda
+            Func<string, string, bool> replaceExtra = (a, b) => a == b;
+            Console.WriteLine(replaceExtra(a, b));
+            }
     }
 }
