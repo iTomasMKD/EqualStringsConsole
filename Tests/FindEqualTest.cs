1cba076631aa629163c3389e876221b9bc39fd3a
Ilijevski7 <tomche.kubikos@hotmail.com>
2022-07-24 10:33:49 +0200

    Add unit tests

---
+++ Tests/FindEqualTest.cs
@@ -0,0 +1,42 @@
+﻿using EqualStringsConsole;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class FindEqualTest
+    {
+        [Test]
+        public void FindEqualHappyPathReturnProperResult()
+          {
+            //Arrange
+            string testA = "xyz";
+            string testB = "xyz";
+
+            //Act
+            var result = GenericEqual.FindEqual(testA, testB);
+
+            //Assert
+            Assert.IsTrue(result);
+          }
+
+        [Test]
+        public void CheckNotEqualPathReturnProperResult()
+        {
+            //Arrange
+            string testA = "xyz";
+            string testB = "xy";
+
+            //Act
+            var result = GenericEqual.FindEqual(testA, testB);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
